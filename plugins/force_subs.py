18afcbc8aefb114ec0c6f322dabddf9218e3d042
•_• <abhinavaditya035@gmail.com>
2025-06-15 11:15:25 +0530

    Update force_subs.py

--- plugins/force_subs.py
+++ plugins/force_subs.py
@@ -2,10 +2,10 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
 from config import Config
-from helper.database import madflixbotz
+from helper.database import codeflixbots
 
 async def not_subscribed(_, client, message):
-    await madflixbotz.add_user(client, message)
+    await codeflixbots.add_user(client, message)
     if not Config.FORCE_SUB:
         return False
     try:             
