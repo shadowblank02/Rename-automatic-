58101b8ae7809aacd1e3203b7b1ce7e0f9dac6a2
•_• <abhinavaditya035@gmail.com>
2025-06-16 15:35:19 +0530

    Update force_subs.py

--- plugins/force_subs.py
+++ plugins/force_subs.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters, enmus
+from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
 from config import Config
@@ -8,29 +8,36 @@ async def not_subscribed(_, client, message):
     await codeflixbots.add_user(client, message)
     if not Config.FORCE_SUB:
         return False
-    try:             
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
+    try:
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
         if user.status == enums.ChatMemberStatus.BANNED:
-            return True 
+            return True
         else:
-            return False                
+            return False
     except UserNotParticipant:
         pass
     return True
 
-
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
     buttons = [
-    [InlineKeyboardButton(text="•ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ•", url=f"https://t.me/{Config.FORCE_SUB}")]
-]
-
-text = "<b>Yᴏᴜ Bᴀᴋᴋᴀᴀ...!! \n<blockquote>Jᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ\n\nᴏᴛʜᴇʀᴡɪsᴇ Yᴏᴜ ᴀʀᴇ ɪɴ ʙɪɢ sʜɪᴛ...!!</blockquote>\nAғᴛᴇʀ Jᴏɪɴɪɴɢ Cʜᴀɴɴᴇʟ ᴄʟɪᴄᴋ ᴏɴ ᴄʟɪᴄᴋ ʜᴇʀᴇ </b>"
-
-try:
-    user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
-    if user.status == enums.ChatMemberStatus.BANNED:
-        return await client.send_message(message.from_user.id, text="Sorry You Are Banned To Use Me")
-    buttons.append([InlineKeyboardButton(text="Cʟɪᴄᴋ ʜᴇʀᴇ", url=f"https://t.me/{bot_username}?start")])
-except UserNotParticipant:
-    return await message.reply_text(f"Yᴏᴜ ʙᴀᴋᴋᴀᴀ...!! Jᴏɪɴ ɪᴛ...", reply_markup=InlineKeyboardMarkup(buttons))
+        [InlineKeyboardButton(text="•ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ•", url=f"https://t.me/{Config.FORCE_SUB}")]
+    ]
+    text = "<b>Yᴏᴜ Bᴀᴋᴋᴀᴀ...!! \n<blockquote>Jᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ\n\nᴏᴛʜᴇʀᴡɪsᴇ Yᴏᴜ ᴀʀᴇ ɪɴ ʙɪɢ sʜɪᴛ...!!</blockquote>\nAғᴛᴇʀ Jᴏɪɴɪɴɢ Cʜᴀɴɴᴇʟ ᴄʟɪᴄᴋ ᴏɴ ᴄʟɪᴄᴋ ʜᴇʀᴇ </b>"
+    try:
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
+        if user.status == enums.ChatMemberStatus.BANNED:
+            return await client.send_message(message.from_user.id, text="Sorry You Are Banned To Use Me")
+        buttons.append([InlineKeyboardButton(text="Cʟɪᴄᴋ ʜᴇʀᴇ", url=f"https://t.me/{Config.BOT_USERNAME}?start")])
+    except UserNotParticipant:
+        if Config.FSUB_PIC:
+            await message.reply_photo(
+                Config.FSUB_PIC,
+                caption=text,
+                reply_markup=InlineKeyboardMarkup(buttons)
+            )
+        else:
+            await message.reply_text(
+                f"Yᴏᴜ ʙᴀᴋᴋᴀᴀ...!! Jᴏɪɴ ɪᴛ...",
+                reply_markup=InlineKeyboardMarkup(buttons)
+            )
