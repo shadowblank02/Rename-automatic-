86eb46c786f3f8fa041d8f3e112f64afa0ea1e9c
•_• <abhinavaditya035@gmail.com>
2025-06-20 11:53:01 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -8,6 +8,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
 ADMIN_USER_ID = Config.ADMIN
 
 # Flag to indicate if the bot is restarting
@@ -19,87 +20,63 @@ async def restart_bot(b, m):
     if not is_restarting:
         is_restarting = True
         await m.reply_text("**Hᴇʏ...!! Oᴡɴᴇʀ/Aᴅᴍɪɴ Jᴜsᴛ ʀᴇʟᴀx ɪᴀᴍ ʀᴇsᴛᴀʀᴛɪɴɢ...!!**")
-
         # Gracefully stop the bot's event loop
         b.stop()
-        time.sleep(2)  # Adjust the delay duration based on your bot's shutdown time
-
+        time.sleep(2)
         # Restart the bot process
         os.execl(sys.executable, sys.executable, *sys.argv)
 
-
 @Client.on_message(filters.private & filters.command(["tutorial"]))
-async def tutorial(bot,message):
-	user_id = message.from_user.id
-	format_template = await codeflixbots.get_format_template(user_id)
-	await message.reply_text(
-	    text =Txt.FILE_NAME_TXT.format(format_template=format_template),
-	    disable_web_page_preview=True,
-	    reply_markup=InlineKeyboardMarkup([
-        			[InlineKeyboardButton("•Sᴜᴘᴘᴏʀᴛ•",url = "https://t.me/BOTSKINGDOMSGROUP"), 
-        			InlineKeyboardButton("•⚡Main hub•",url = "https://t.me/botskingdoms") ]])
-	)
-
+async def tutorial(bot, message):
+    user_id = message.from_user.id
+    format_template = await codeflixbots.get_format_template(user_id)
+    await message.reply_text(
+        text=Txt.FILE_NAME_TXT.format(format_template=format_template),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("•Sᴜᴘᴘᴏʀᴛ•", url="https://t.me/BOTSKINGDOMSGROUP"), InlineKeyboardButton("•⚡Main hub•", url="https://t.me/botskingdoms")]
+        ])
+    )
 
 @Client.on_message(filters.command(["stats", "status"]) & filters.user(Config.ADMIN))
 async def get_stats(bot, message):
     total_users = await codeflixbots.total_users_count()
-    uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - bot.uptime))    
+    uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - bot.uptime))
     start_t = time.time()
-    st = await message.reply('**Accessing The Details.....**')    
+    st = await message.reply('**Accessing The Details.....**')
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
     await st.edit(text=f"**--Bot Status--** \n\n**⌚️ Bot Uptime :** {uptime} \n**🐌 Current Ping :** `{time_taken_s:.3f} ms` \n**👭 Total Users :** `{total_users}`")
 
 @Client.on_message(filters.command("broadcast") & filters.user(Config.ADMIN) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
-    await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Is Started The Broadcast......")
-    all_users = await codeflixbots.get_all_users()
-    broadcast_msg = m.reply_to_message
-    sts_msg = await m.reply_text("Broadcast Started..!") 
-    done = 0
-    failed = 0
-    success = 0
-    start_time = time.time()
-    total_users = await codeflixbots.total_users_count()
-    async for user in all_users:
-        sts = await send_msg(user['_id'], broadcast_msg)
-        if sts == 200:
-           success += 1
-        else:
-           failed += 1
-        if sts == 400:
-           await codeflixbots.delete_user(user['_id'])
-        done += 1
-        if not done % 20:
-           await sts_msg.edit(f"Broadcast In Progress: \n\nTotal Users {total_users} \nCompleted : {done} / {total_users}\nSuccess : {success}\nFailed : {failed}")
-    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Cᴏᴍᴩʟᴇᴛᴇᴅ: \nCᴏᴍᴩʟᴇᴛᴇᴅ Iɴ `{completed_in}`.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
-           
-async def send_msg(user_id, message):
     try:
-        await message.copy(chat_id=int(user_id))
-        return 200
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return send_msg(user_id, message)
-    except InputUserDeactivated:
-        logger.info(f"{user_id} : Deactivated")
-        return 400
-    except UserIsBlocked:
-        logger.info(f"{user_id} : Blocked The Bot")
-        return 400
-    except PeerIdInvalid:
-        logger.info(f"{user_id} : User ID Invalid")
-        return 400
+        await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Is Started The Broadcast......")
+        all_users = await codeflixbots.get_all_users()
+        broadcast_msg = m.reply_to_message
+        sts_msg = await m.reply_text("Bʀᴏᴀᴅᴄᴀsᴛ sᴛᴀʀᴛᴇᴅ ᴅᴜᴅᴇ..!! >_<")
+        done = 0
+        failed = 0
+        success = 0
+        start_time = time.time()
+        total_users = await codeflixbots.total_users_count()
+        for user in all_users:
+            try:
+                await send_msg(user['_id'], broadcast_msg)
+                success += 1
+            except Exception as e:
+                # failed += 1
+                logger.error(f"Error broadcasting to {user['_id']}: {e}")
+            done += 1
+            if done % 20 == 0:
+                await sts_msg.edit(f"Broadcast In Progress: \n\nTotal Users {total_users} \nCompleted : {done} / {total_users}\nSuccess : {success}\nFailed : {failed}")
+        completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
+        await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Cᴏᴍᴩʟᴇᴛᴇᴅ: \nCᴏᴍᴩʟᴇᴛᴇᴅ Iɴ `{completed_in}`.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed : {failed}")
     except Exception as e:
-        logger.error(f"{user_id} : {e}")
-        return 500
+        logger.error(f"Error in broadcast handler: {e}")
 
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
+async def send_msg(user_id, message):
+    while True:
+        try:
+            await message.copy(chat_id=int(user_id))
+            return 200
