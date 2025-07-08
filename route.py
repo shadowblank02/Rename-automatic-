726d972f1a5e001ceabe874dad833af0c25e3086
•_• <abhinavaditya035@gmail.com>
2025-07-08 22:49:21 +0530

    Update route.py

--- route.py
+++ route.py
@@ -4,17 +4,10 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Madflix_Bots")
+    return web.json_response("Botskingdom")
 
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
