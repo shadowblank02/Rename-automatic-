11649dc6d8068388d44ab0c3aa6934a89e0c4cbe
•_• <abhinavaditya035@gmail.com>
2025-07-09 15:00:11 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from helper.database import codeflixbots
+from helper.database import Botskingdom
 from config import Config
 from functools import wraps
 
@@ -10,7 +10,7 @@ def check_ban(func):
     @wraps(func)
     async def wrapper(client, message, *args, **kwargs):
         user_id = message.from_user.id
-        user = await codeflixbots.col.find_one({"_id": user_id})
+        user = await Botskingdom.col.find_one({"_id": user_id})
         if user and user.get("ban_status", {}).get("is_banned", False):
             keyboard = InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ...!!", url=ADMIN_URL)]]
@@ -28,22 +28,22 @@ async def add_caption(client, message):
     if len(message.command) == 1:
        return await message.reply_text("**Give The Caption\n\nExample :- `/set_caption 📕Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
     caption = message.text.split(" ", 1)[1]
-    await codeflixbots.set_caption(message.from_user.id, caption=caption)
+    await Botskingdom.set_caption(message.from_user.id, caption=caption)
     await message.reply_text("**Your Caption Successfully Added ✅**")
    
 @Client.on_message(filters.private & filters.command('del_caption'))
 @check_ban
 async def delete_caption(client, message):
-    caption = await codeflixbots.get_caption(message.from_user.id)  
+    caption = await Botskingdom.get_caption(message.from_user.id)  
     if not caption:
        return await message.reply_text("**You Don't Have Any Caption ❌**")
-    await codeflixbots.set_caption(message.from_user.id, caption=None)
+    await Botskingdom.set_caption(message.from_user.id, caption=None)
     await message.reply_text("**Your Caption Successfully Deleted 🗑️**")
                                        
 @Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
 @check_ban
 async def see_caption(client, message):
-    caption = await codeflixbots.get_caption(message.from_user.id)  
+    caption = await Botskingdom.get_caption(message.from_user.id)  
     if caption:
        await message.reply_text(f"**Your Caption :**\n\n`{caption}`")
     else:
@@ -53,7 +53,7 @@ async def see_caption(client, message):
 @Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
 @check_ban
 async def viewthumb(client, message):    
-    thumb = await codeflixbots.get_thumbnail(message.from_user.id)
+    thumb = await Botskingdom.get_thumbnail(message.from_user.id)
     if thumb:
        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
@@ -62,21 +62,12 @@ async def viewthumb(client, message):
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 @check_ban
 async def removethumb(client, message):
-    await codeflixbots.set_thumbnail(message.from_user.id, file_id=None)
+    await Botskingdom.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("**Thumbnail Deleted Successfully 🗑️**")
 	
 @Client.on_message(filters.private & filters.photo)
 @check_ban
 async def addthumbs(client, message):
     mkn = await message.reply_text("Please Wait ...")
-    await codeflixbots.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await Botskingdom.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
     await mkn.edit("**Thumbnail Saved Successfully ✅️**")
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
