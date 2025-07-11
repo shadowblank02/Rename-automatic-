eaf2522f54bafe585079afdded6aa94a76eb891a
•_• <abhinavaditya035@gmail.com>
2025-07-11 06:49:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,6 @@ class Bot(Client):
         if Config.WEBHOOK:
             app = web.AppRunner(await web_server())
             await app.setup()
-            port = int(os.environ.get("PORT", 8080))
             await web.TCPSite(app, "0.0.0.0", port).start()
         print(f"{me.first_name} Is Started.....✨️")
         uptime_seconds = int(time.time() - self.start_time)
