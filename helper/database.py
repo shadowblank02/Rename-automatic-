f775f6ecc1bd724561de2fb3157cf5d547f8e46b
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-07-18 19:13:17 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -36,11 +36,11 @@ class Database:
     async def add_user(self, b, m):
         u = m.from_user
         if not await self.is_user_exist(u.id):
-            user = self.new_user(u.id, u.username)
+            user = self.new_user(u.id)
             try:
                 await self.col.insert_one(user)
                 await send_log(b, u)
-
+            except Exception as e:
                 logging.error(f"Error adding user {u.id}: {e}")
 
     async def is_user_exist(self, id):
@@ -142,61 +142,60 @@ class Database:
 
     async def get_title(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('title', 'Bots kingdom')
+        return user.get('title', 'Botskingdom')
 
     async def set_title(self, user_id, title):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'title': title}})
 
     async def get_author(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('author', 'Botskingdoms')
+        return user.get('author', 'Botskingdom')
 
     async def set_author(self, user_id, author):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'author': author}})
 
     async def get_artist(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('artist', 'Botskingdoms')
+        return user.get('artist', 'Botskingdom')
 
     async def set_artist(self, user_id, artist):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'artist': artist}})
 
     async def get_audio(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('audio', 'Bots kingdom')
+        return user.get('audio', 'Botskingdom')
 
     async def set_audio(self, user_id, audio):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'audio': audio}})
 
     async def get_subtitle(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('subtitle', "Botskingdoms")
+        return user.get('subtitle', "Botskingdom")
 
     async def set_subtitle(self, user_id, subtitle):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'subtitle': subtitle}})
 
     async def get_video(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('video', 'Botskingdoms')
+        return user.get('video', 'Botskingdom')
 
     async def set_video(self, user_id, video):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'video': video}})
 
     async def get_encoded_by(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('encoded_by', "Botskingdoms")
+        return user.get('encoded_by', "Botskingdom")
 
     async def set_encoded_by(self, user_id, encoded_by):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'encoded_by': encoded_by}})
-
+        
     async def get_custom_tag(self, user_id):
         user = await self.col.find_one({'_id': int(user_id)})
-        return user.get('customtag', "Botskingdoms")
+        return user.get('customtag', "Botskingdom")
 
     async def set_custom_tag(self, user_id, custom_tag):
         await self.col.update_one({'_id': int(user_id)}, {'$set': {'custom_tag': custom_tag}})
-
-    # Example methods to add in helper/database.py
+# Example methods to add in helper/database.py
 
     async def ban_user(user_id):
         await db.banned_users.update_one({"_id": user_id}, {"$set": {"_id": user_id}}, upsert=True)
@@ -209,7 +208,3 @@ class Database:
 
     async def get_banned_users():
         return db.banned_users.find()
-
-
-
-Botskingdom = Database(Config.DB_URL, Config.DB_NAME)
