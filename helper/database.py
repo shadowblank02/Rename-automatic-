70e9aeb693bf3b26fcff25a49e017ee1a40fd888
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-07-18 19:22:05 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -36,7 +36,7 @@ class Database:
     async def add_user(self, b, m):
         u = m.from_user
         if not await self.is_user_exist(u.id):
-            user = self.new_user(u.id)
+            user = self.new_user(u.id, u.username)
             try:
                 await self.col.insert_one(user)
                 await send_log(b, u)
